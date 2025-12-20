e817c0839a79e17d3ecf0564267d4bdf639b5845
anas-roshdi <a7nas.talika@gmail.com>
2025-12-20 07:10:27 -0500

    Finished the project

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
+          // 'cart' is the name of the slice in the store, and it's managed by cartReducer
         cart: cartReducer,
     },
 });
